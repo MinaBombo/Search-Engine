03317569484a0e9c777031f8839605a4e0957796
MinaBombo <minasamy_@hotmail.com>
2018-02-26 12:31:31 +0200

    Spider class almost finished

--- src/main/java/crawler/Leg.java
+++ src/main/java/crawler/Leg.java
@@ -2,8 +2,17 @@ package crawler;
 
 public class Leg extends Thread{
 
+    private final Spider parentSpider;
+
+    Leg(Spider spider){
+        parentSpider = spider;
+    }
+
+
     @Override
     public void run() {
-        super.run();
+        String url = parentSpider.getNextUrl();
+        // TODO: Download page, extract urls and add to graph
+        // TODO: Add newly extracted urls to parentSpider
     }
 }
--- src/main/java/crawler/Spider.java
+++ src/main/java/crawler/Spider.java
@@ -1,24 +1,20 @@
 package crawler;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.io.*;
+import java.util.*;
 
 public class Spider {
     private int maxNumPages;
     private int maxNumThreads;
 
-    private Set<String>  visitedPages;
-    private List<String> toBeVisitedPages;
+    private Set<String> visitedPages;
+    private LinkedList<String> toBeVisitedPages;
 
-    private Map<String, String> graphBuilder;
     private final static String resourcesDir  = "../../resources/";
-    private final static String graphFileName = "Graph.txt";
+    private final static String privateFolderName = "crawler";
+    private final static String graphFileName = "graph.txt";
+    private static int crawlerCount = 0;
+    private BufferedWriter bufferedWriter;
 
     public Spider(int numPages, int numThreads){
         maxNumPages = numPages;
@@ -26,7 +22,7 @@ public class Spider {
     }
 
     private void seedList(String seedFilePath){
-        toBeVisitedPages = new ArrayList<>();
+        toBeVisitedPages = new LinkedList<>();
         try {
             File file = new File(seedFilePath);
             FileReader fileReader = new FileReader(file);
@@ -34,7 +30,7 @@ public class Spider {
 
             String url;
             while ((url = bufferedReader.readLine()) != null)
-                toBeVisitedPages.add(url);
+                toBeVisitedPages.addFirst(url);
 
             fileReader.close();
         } catch (IOException e) {
@@ -42,13 +38,68 @@ public class Spider {
         }
     }
 
+    private void createGraphFile(){
+        try {
+            Writer fileWriter = new FileWriter(
+                    resourcesDir+privateFolderName+String.valueOf(crawlerCount++)+graphFileName);
+            bufferedWriter = new BufferedWriter(fileWriter);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void closeGraphFile(){
+        if (bufferedWriter != null) {
+            try {
+                bufferedWriter.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    synchronized String getNextUrl(){
+        String url;
+        url = toBeVisitedPages.getLast();
+        toBeVisitedPages.removeLast();
+        visitedPages.add(url);
+        return url;
+    }
+
+    synchronized void addUrlsToVisit(String... urls){
+        for (String url: urls) {
+            if(!visitedPages.contains(url))
+                visitedPages.add(url);
+        }
+    }
+
+    synchronized void addEdgesToGraph(String urlPointing, String... urlsPointedAt){
+        for (String url: urlsPointedAt){
+            try {
+                bufferedWriter.write(urlPointing + " " + url);
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+    }
+
     public void crawl(String seedFilePath){
         seedList(seedFilePath);
+
         Leg[] legs = new Leg[maxNumThreads];
         for(int i=0; i<maxNumThreads; ++i){
-            legs[i] = new Leg();
+            legs[i] = new Leg(this);
             legs[i].run();
         }
+
+        for (Leg leg:legs) {
+            try{
+                leg.join();
+            } catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+        closeGraphFile();
     }
 
 }
