d9fbaa88d1b6435b0dd242eee1692c52e8d55006
mina <minasamy_@hotmail.com>
2018-03-29 22:35:56 +0200

    Refactored Crawler parameters

--- Crawler/src/main/java/crawler/WebCrawler.java
+++ Crawler/src/main/java/crawler/WebCrawler.java
@@ -2,7 +2,6 @@ package crawler;
 
 import Database.ConnectionPool;
 import Database.DatabaseController;
-import Indexer.DynamicIndexer;
 import Util.Seed;
 
 import java.sql.SQLException;
@@ -15,13 +14,25 @@ import java.util.concurrent.Future;
 
 public class WebCrawler {
 
+    private final static int maxNumUrls = 5000;
+    private static int maxNumThreads;
+
     public static void main(String[] args) throws SQLException {
+        try{
+            maxNumThreads = Integer.parseInt(args[0]);
+        } catch (Exception e){
+            System.err.println("Error while parsing arguments");
+            System.err.println(e.toString());
+            System.err.println("Setting parameters to default values");
+            maxNumThreads = Runtime.getRuntime().availableProcessors();
+        }
+
         DatabaseController controller = new DatabaseController();
-        ExecutorService pool = Executors.newFixedThreadPool(DynamicIndexer.numThreads);
+        ExecutorService pool = Executors.newFixedThreadPool(maxNumThreads);
         List<Seed> seeds;
         int processedURLCount = 0;
         do {
-            seeds = controller.getUnprocessedSeeds(DynamicIndexer.numThreads, 0);
+            seeds = controller.getUnprocessedSeeds(maxNumThreads, 0);
             List<WebCrawlingTask> tasks = new ArrayList<>();
             for (Seed seed : seeds) {
                 tasks.add(new WebCrawlingTask(seed));
@@ -38,7 +49,7 @@ public class WebCrawler {
                 exception.printStackTrace();
             }
             System.out.println(processedURLCount);
-        } while (!seeds.isEmpty() && processedURLCount <= 5000);
+        } while (!seeds.isEmpty() && processedURLCount <= maxNumUrls);
         pool.shutdown();
         controller.close();
         try {
