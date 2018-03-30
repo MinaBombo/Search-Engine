ff7341b37d2fa896cb10a61b58ca7bd5bba8255a
Hassan <hsaied117@yahoo.com>
2018-03-30 20:46:54 +0200

    H5rg mel2oda xD

--- Utils/src/main/java/Database/SeedDatabaseModule.java
+++ Utils/src/main/java/Database/SeedDatabaseModule.java
@@ -5,59 +5,63 @@ import BusinessModel.Seed;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
 public class SeedDatabaseModule implements DatabaseModule<List<Seed>> {
     DatabaseConnector connector;
 
-    SeedDatabaseModule(DatabaseConnector connector){
+    SeedDatabaseModule(DatabaseConnector connector) {
         this.connector = connector;
     }
-
 
 
     @Override
     public void update(List<Seed> seeds) throws SQLException {
 
     }
-    public void update(Seed seed) throws SQLException{
-        String sqlStatement = "UPDATE "+DatabaseColumn.SEED +" SET URL = ?, Processed = ? WHERE ID = ?";
+
+    public void update(Seed seed) throws SQLException {
+        String sqlStatement = "UPDATE " + DatabaseColumn.SEED + " SET URL = ?, Processed = ? , InLinks = ?  WHERE ID = ?";
         PreparedStatement statement = connector.getPooledConnection().prepareStatement(sqlStatement);
-        statement.setString(1,seed.getUrl());
-        statement.setBoolean(2,seed.isProcessed());
-        statement.setInt(3,seed.getId());
+        statement.setString(1, seed.getUrl());
+        statement.setBoolean(2, seed.isProcessed());
+        statement.setInt(3, seed.getInLinks());
+        statement.setInt(4, seed.getId());
         statement.executeUpdate();
         statement.close();
     }
+
     @Override
     public void insert(List<Seed> seeds) throws SQLException {
-        String sqlStatement = "INSERT INTO "+ DatabaseColumn.SEED + "(URL, Processed) VALUES (?,?) ON CONFLICT (URL) DO NOTHING";
+        String sqlStatement = "INSERT INTO " + DatabaseColumn.SEED + "(URL, Processed , InLinks) VALUES (?,?,?) ON CONFLICT (URL) DO UPDATE SET InLinks = EXCLUDED.InLinks+1";
         PreparedStatement statement = connector.getPooledConnection().prepareStatement(sqlStatement);
-        for(Seed seed : seeds) {
+        for (Seed seed : seeds) {
             statement.setString(1, seed.getUrl());
-            statement.setBoolean(2,seed.isProcessed());
+            statement.setBoolean(2, seed.isProcessed());
+            statement.setInt(3, seed.getInLinks());
             statement.addBatch();
         }
 
         statement.executeBatch();
         ResultSet resultSet = statement.getGeneratedKeys();
         int counter = 0;
-        while (resultSet.next()){
+        while (resultSet.next()) {
             seeds.get(counter++).setId(resultSet.getInt(1));
         }
         statement.close();
     }
 
-    public List<Seed> select(int limit,int offset) throws SQLException{
+    public List<Seed> select(int limit, int offset) throws SQLException {
         List<Seed> seeds = new ArrayList<>();
-        String sqlStatement = "SELECT ID , URL, Processed FROM "+DatabaseColumn.SEED+" WHERE Processed = FALSE LIMIT ? OFFSET ?";
+        String sqlStatement = "SELECT ID , URL, Processed, InLinks FROM " + DatabaseColumn.SEED + " WHERE Processed = FALSE LIMIT ? OFFSET ?";
         PreparedStatement statement = connector.getPooledConnection().prepareStatement(sqlStatement);
-        statement.setInt(1,limit);
-        statement.setInt(2,offset);
+        statement.setInt(1, limit);
+        statement.setInt(2, offset);
         ResultSet resultSet = statement.executeQuery();
-        while (resultSet.next()){
-            seeds.add(new Seed(resultSet.getInt(1),resultSet.getString(2),resultSet.getBoolean(3)));
+        while (resultSet.next()) {
+            seeds.add(new Seed(resultSet.getInt(1), resultSet.getString(2), resultSet.getBoolean(3), resultSet.getInt(4)));
         }
         statement.close();
         return seeds;
@@ -72,11 +76,25 @@ public class SeedDatabaseModule implements DatabaseModule<List<Seed>> {
     public List<List<Seed>> select() throws SQLException {
         return null;
     }
-    public void delete(Seed seed) throws SQLException{
-        String sqlStatement = "DELETE FROM "+ DatabaseColumn.SEED + " WHERE ID = ?";
+
+    public void delete(Seed seed) throws SQLException {
+        String sqlStatement = "DELETE FROM " + DatabaseColumn.SEED + " WHERE ID = ?";
         PreparedStatement statement = connector.getPooledConnection().prepareStatement(sqlStatement);
-        statement.setInt(1,seed.getId());
+        statement.setInt(1, seed.getId());
         statement.executeUpdate();
+        statement.close();
+    }
+
+    public void refresh() throws SQLException {
+        String sqlStatement = "INSERT INTO TempSeedStorage SELECT * FROM ((SELECT * FROM SEED s1 WHERE s1.processed = true ORDER BY RANDOM() LIMIT 2500 )UNION (SELECT * FROM SEED s2 WHERE s2.processed = false ORDER BY RANDOM() LIMIT 2500)) SeedSelection;\n";
+        Statement statement = connector.getPooledConnection().createStatement();
+        statement.execute(sqlStatement);
+        sqlStatement = "DELETE FROM Seed;";
+        statement.execute(sqlStatement);
+        sqlStatement = "INSERT INTO Seed Select * FROM TempSeedStorage";
+        statement.execute(sqlStatement);
+        sqlStatement = "DELETE FROM TempSeedStorage";
+        statement.execute(sqlStatement);
         statement.close();
     }
 }
