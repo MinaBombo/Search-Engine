a1c18b01b92e2f1cbaca08fc39c472113507eff7
MinaBombo <minasamy_@hotmail.com>
2018-04-19 16:48:06 +0200

    Renamed DynamicIndexer to Indexer

--- Indexer/src/main/java/Indexer/Indexer.java
+++ Indexer/src/main/java/Indexer/Indexer.java
@@ -11,7 +11,7 @@ import java.util.concurrent.*;
 
 import static Tools.ThreadCounter.getNumThreads;
 
-public class DynamicIndexer {
+public class Indexer {
 
 
     public static void main(String[] args) throws SQLException, InterruptedException {
