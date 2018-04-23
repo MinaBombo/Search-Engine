d97e0a0d860c6183c9498e6a1b18a2324c2d9aaa
mina <minasamy_@hotmail.com>
2018-04-24 01:59:40 +0200

    Set Browser module structure and added TODOs

--- Browser/src/main/java/browser/DynamicRanker.java
+++ Browser/src/main/java/browser/DynamicRanker.java
@@ -1,4 +1,43 @@
 package browser;
 
+import BusinessModel.Document;
+
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Logger;
+
 public class DynamicRanker {
+
+    private static Logger logger = Logger.getLogger(DynamicRanker.class.getName());
+    private List<RankingDocument> rankingDocsList = null;
+    private Map<String, Integer> idfMap = null;
+
+    private void getRelevantData(){
+        // TODO: populates docsList from database
+        // TODO: computes idfMap: each word occurs in how many doc
+        // idf[searchWord] = log(total docs number / number of docs containing searchWord)
+    }
+
+    private void processSearchWord(String searchWord){
+        double wordIdf = idfMap.get(searchWord);
+        for(RankingDocument rankingDocument:rankingDocsList){
+            rankingDocument.addToDynamicRank(wordIdf * rankingDocument.getNormWordFreq(searchWord));
+        }
+    }
+
+    private List<Document> getFinalSortedDocs(){
+        // TODO: Fetch the actual documents by id from rankingDocsList
+        List<Document> finalDocsList = null;
+        return finalDocsList;
+    }
+
+    public List<Document> rank(String searchWords[]){
+        getRelevantData();
+        for(String searchWord:searchWords){
+            processSearchWord(searchWord);
+        }
+        rankingDocsList.sort(RankingDocument::compareTo);
+        return getFinalSortedDocs();
+    }
+
 }
