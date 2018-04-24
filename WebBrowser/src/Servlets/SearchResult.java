5533139a091b602ededd0c0342c06e6e24b84b43
mina <minasamy_@hotmail.com>
2018-04-25 01:50:16 +0200

    Fixed Gradle-iml integration

--- WebBrowser/src/Servlets/SearchResult.java
+++ WebBrowser/src/Servlets/SearchResult.java
@@ -1,17 +1,25 @@
 package Servlets;
 
+import BusinessModel.BrowserDocument;
+import DynamicRanker.DynamicRanker;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet(urlPatterns = "/search_result.html")
 public class SearchResult extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-
+        DynamicRanker dynamicRanker = new DynamicRanker();
+        final String keywords[] = req.getParameter("keywords").split(" ");
+        List<BrowserDocument> browserDocumentList = dynamicRanker.rank(keywords);
+        // TODO: show urls and parts of documents
+        // TODO: handle url redirection
     }
 }
