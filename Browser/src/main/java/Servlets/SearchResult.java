b66e962a0b64490b3d494d9d944d634cfcf5bdea
Hassan <hsaied117@yahoo.com>
2018-04-25 16:09:07 +0200

    Temp GUI done

--- Browser/src/main/java/Servlets/SearchResult.java
+++ Browser/src/main/java/Servlets/SearchResult.java
@@ -13,7 +13,7 @@ import java.io.PrintWriter;
 import java.util.List;
 import java.util.NoSuchElementException;
 
-@WebServlet(urlPatterns = "/search_result.html")
+@WebServlet(urlPatterns = "/search_result")
 public class SearchResult extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
@@ -29,16 +29,30 @@ public class SearchResult extends HttpServlet {
             return;
         }
         List<BrowserDocument> browserDocumentList = dynamicRanker.getBrowserDocuments(pageNumber);
-        // TODO: show urls and parts of documents
-        // TODO: handle url redirection
         try (PrintWriter printWriter = resp.getWriter()){
-            for(BrowserDocument browserDocument:browserDocumentList){
-                printWriter.println(browserDocument.getUrl());
-                printWriter.println(browserDocument.getDescription());
+            if(browserDocumentList == null){
+                printWriter.println("No more results!");
+                return;
             }
+            for(BrowserDocument browserDocument:browserDocumentList){
+                printWriter.println("<a href='"+browserDocument.getUrl()+"'>"+browserDocument.getUrl()+"</a><br>");
+                printWriter.println(browserDocument.getDescription()+"<br><br>");
+            }
+            printWriter.println(
+                    "<script>\n" +
+                    "    function next(){\n" +
+                    "        location.href='search_result?keywords=" + req.getParameter("keywords") + "&page=" + String.valueOf(pageNumber+1) +"';" +
+                    "    }\n" +
+                    "</script>" +
+                    "<div align=\"center\">\n" +
+                    "    <form action=\"javascript:next();\">\n" +
+                    "        <input type=\"submit\" value=\"Next\">\n" +
+                    "    </form>\n" +
+                    "</div>");
         }catch (IOException e){
             e.printStackTrace();
         }
     }
+
 }
 
